4b0d11a6230d2e529668ee1cbdb1bbd5c95a9f82
Alvaro Maceda <alvaro@alvaromaceda.es>
2016-10-27 08:49:30 +0200

    Adds PHPUnit

---
+++ src/TwigComposer.php
@@ -0,0 +1,8 @@
+<?php
+namespace TwigComposer;
+
+
+class TwigComposer
+{
+
+}
---
+++ tests/TwigComposerTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace TwigComposerTests;
+
+
+class TwigComposerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $foo = false;
+        $this->assertTrue($foo);
+    }
+}
