a86061c67a3e66772906b743eadb809d1bfbb34e
Alvaro Maceda <alvaro@alvaromaceda.es>
2016-10-27 10:03:01 +0200

    Creation of class TWigComposer

--- src/TwigComposer.php
+++ src/TwigComposer.php
@@ -2,7 +2,31 @@
 namespace TwigComposer;
 
 
-class TwigComposer
+class TwigComposer extends \Twig_Template
 {
+//    public function render(array $context)
+//    {
+//        //vendor/laravel/framework/src/Illuminate/Events/Dispatcher.php tiene un dispatcher
+//        $debug = var_export($context, true);
+//        $trace = '<h1>OLA K ASE</h1>'.$debug."<br>".get_class($this)."<br>".$this->getTemplateName();
+//        $trace .= "<pre>".var_export($this->env->getLoader(), true)."</pre>";
+//        $trace .= "<pre>".var_export($this->env->getLoader()->getPaths('test1'), true)."</pre>";
+//        return str_replace(
+//            "</body>",
+//            $trace."\n</body>",
+//            parent::render($context)
+//        );
+//    }
 
+    // Never called: it's overriden in child-generated classes and
+    // the override method does not call parent's
+    public function getTemplateName()
+    {
+    }
+
+    // Never called: it's overriden in child-generated classes and
+    // the override method does not call parent's
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+    }
 }
--- tests/TwigComposerTest.php
+++ tests/TwigComposerTest.php
@@ -1,12 +1,50 @@
 <?php
 namespace TwigComposerTests;
 
+use TwigComposer\TwigComposer;
 
 class TwigComposerTest extends \PHPUnit_Framework_TestCase
 {
+    protected $twig;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $loader1 = new \Twig_Loader_Array(array(
+            'base' => '{% block content %}{% endblock %}',
+        ));
+        $loader2 = new \Twig_Loader_Array(array(
+            'index' => '{% extends "base.html" %}{% block content %}Hello {{ name }}{% endblock %}',
+            'base'  => 'Will never be loaded',
+        ));
+
+        $loader = new \Twig_Loader_Chain(array($loader1, $loader2));
+
+        // Twig as renderer
+        $this->twig = new \Twig_Environment(
+            $loader,
+            array(
+                'debug' => true,
+                'base_template_class' => 'TwigComposer\TwigComposer',
+                'cache' => 'tests/twig_tmp',
+            ));
+    }
+
     public function testTrueIsTrue()
     {
-        $foo = false;
+        $foo = true;
         $this->assertTrue($foo);
+    }
+
+    public function testClassInstantiation()
+    {
+        $instance = new TwigComposer($this->twig);
+        $this->assertNotNull($instance);
+    }
+
+    public function testTemplateRenders()
+    {
+        $this->twig->render('base');
     }
 }
