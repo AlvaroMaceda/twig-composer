53541973e15083ad15d74c51a977adb2995c96a0
Alvaro Maceda <alvaro@alvaromaceda.es>
2016-10-31 13:37:13 +0100

    Adds context to the tests

--- tests/TwigComposerTest.php
+++ tests/TwigComposerTest.php
@@ -12,6 +12,11 @@ function callbackFunction($context) {
 class TwigComposerTest extends \PHPUnit_Framework_TestCase
 {
     const FIXTURES_DIRECTORY = 'tests/fixtures';
+    const AN_EXAMPLE_CONTEXT = array(
+        'AVariable' => 10,
+        'AnotherVariable' => 'Hello',
+        'OneMoreVariable' => ['foo','bar']
+        );
 
     protected $twig;
     protected static $callbackFunctionMock;
@@ -50,7 +55,7 @@ class TwigComposerTest extends \PHPUnit_Framework_TestCase
 
     public function callbackFunction($context)
     {
-        self::$callbackFunctionMock->callbackFunction();
+        self::$callbackFunctionMock->callbackFunction($context);
     }
 
     public function setUp()
@@ -80,7 +85,7 @@ class TwigComposerTest extends \PHPUnit_Framework_TestCase
      *
      * @dataProvider provider_Templates_Renders_As_Expected
      */
-    public function xtest_Template_Renders_As_Expected($template, $expected)
+    public function test_Template_Renders_As_Expected($template, $expected)
     {
         $rendered = $this->twig->render($template);
         $expected = $this->loadRenderedFixture($expected);
@@ -101,19 +106,22 @@ class TwigComposerTest extends \PHPUnit_Framework_TestCase
      *
      * @dataProvider providerTemplateList
      */
-    public function test_A__Method_Is_Called_When_Template_Renders($template)
+    public function test_A_Method_Is_Called_When_Template_Renders($template)
     {
         $mock = $this->createMockObjectWithACoupleOfMethods(['method1','method2']);
+        $context = self::AN_EXAMPLE_CONTEXT;
 
         TwigComposer::getNotifier()->on($template, [$mock,'method1']);
 
         $mock->expects($this->once())
-            ->method('method1');
+            ->method('method1')
+            ->with($this->equalTo($context));
 
         $mock->expects($this->never())
-            ->method('method2');
+            ->method('method2')
+            ->with($this->equalTo($context));
 
-        $this->twig->render($template);
+        $this->twig->render($template,$context);
     }
 
     public function providerTemplateList()
@@ -127,59 +135,66 @@ class TwigComposerTest extends \PHPUnit_Framework_TestCase
     public function test_A_Function_Is_Called_When_Template_Renders()
     {
         $template = 'base.twig';
+        $context = self::AN_EXAMPLE_CONTEXT;
 
         TwigComposer::getNotifier()->on($template, 'TwigComposerTests\callbackFunction');
 
         self::$callbackFunctionMock->expects($this->once())
-            ->method('callbackFunction');
+            ->method('callbackFunction')
+            ->with($this->equalTo($context));
 
-        $this->twig->render($template);
+        $this->twig->render($template,$context);
     }
 
-    // http://twig.sensiolabs.org/doc/templates.html#including-other-templates
-    // include -> {{ include('sidebar.html') }} (have access to the same context)
+    // include
     public function test_It_Notifies_When_Parent_Is_Rendered_And_We_Are_Listening_On_Included_Template()
     {
         $includes_template = 'includes.twig';
         $included_template = 'included_template.twig';
+        $context = self::AN_EXAMPLE_CONTEXT;
 
         $mock = $this->createMockObjectWithACoupleOfMethods(['method1','method2']);
 
         $mock->expects($this->once())
-            ->method('method1');
+            ->method('method1')
+            ->with($this->equalTo($context));
 
         TwigComposer::getNotifier()->on($included_template, [$mock,'method1']);
 
-        $this->twig->render($includes_template);
+        $this->twig->render($includes_template,$context);
     }
 
-    // embed
+    // "embed": This one is tricky because Twig uses auxiliar classes with same templatename
     public function test_It_Notifies_Embeding_And_Embedded_Template_Only_Once()
     {
         $embedded_template = 'embedded_template.twig';
         $embeds_template = 'embeds.twig';
+        $context = self::AN_EXAMPLE_CONTEXT;
 
         $mock_embedded = $this->createMockObjectWithACoupleOfMethods(['embedded']);
         $mock_embeds = $this->createMockObjectWithACoupleOfMethods(['embeds']);
 
         $mock_embedded->expects($this->once())
-            ->method('embedded');
+            ->method('embedded')
+            ->with($this->equalTo($context));
 
         $mock_embeds->expects($this->once())
-            ->method('embeds');
+            ->method('embeds')
+            ->with($this->equalTo($context));
 
         TwigComposer::getNotifier()->on($embedded_template, [$mock_embedded,'embedded']);
         TwigComposer::getNotifier()->on($embeds_template, [$mock_embeds,'embeds']);
 
-        $this->twig->render($embeds_template);
+        $this->twig->render($embeds_template,$context);
     }
 
-    // "import" is used for macros: it should not notify when importing
+    // "import" is used for macros: it should NOT notify when importing
     public function test_It_Not_Notifies_Imported_Templates()
     {
         $imported_template = 'imported_template.twig';
         $imports_template_all = 'imports_all_macros.twig';
         $imports_template_individually = 'imports_macros_individually.twig';
+        $context = self::AN_EXAMPLE_CONTEXT;
 
         $mock = $this->createMockObjectWithACoupleOfMethods(['imports_all','imports_individually','imported']);
 
@@ -189,15 +204,17 @@ class TwigComposerTest extends \PHPUnit_Framework_TestCase
 
 
         $mock->expects($this->once())
-            ->method('imports_all');
+            ->method('imports_all')
+            ->with($this->equalTo($context));
         $mock->expects($this->once())
-            ->method('imports_individually');
+            ->method('imports_individually')
+            ->with($this->equalTo($context));
         $mock->expects($this->never())
             ->method('imported');
 
-        $this->twig->render($imports_template_individually);
+        $this->twig->render($imports_template_individually,$context);
 
-        $this->twig->render($imports_template_all);
+        $this->twig->render($imports_template_all,$context);
     }
 
     // extends -> {% extends "base.html" %} {% extends "base.html" %}
@@ -205,20 +222,23 @@ class TwigComposerTest extends \PHPUnit_Framework_TestCase
     {
         $extended_template = 'extended_template.twig';
         $extends_template = 'extends.twig';
+        $context = self::AN_EXAMPLE_CONTEXT;
 
         $mock_extended = $this->createMockObjectWithACoupleOfMethods(['extended']);
         $mock_extends = $this->createMockObjectWithACoupleOfMethods(['extends']);
 
         $mock_extended->expects($this->once())
-            ->method('extended');
+            ->method('extended')
+            ->with($this->equalTo($context));
 
         $mock_extends->expects($this->once())
-            ->method('extends');
+            ->method('extends')
+            ->with($this->equalTo($context));
 
         TwigComposer::getNotifier()->on($extended_template, [$mock_extended,'extended']);
         TwigComposer::getNotifier()->on($extends_template, [$mock_extends,'extends']);
 
-        $this->twig->render($extends_template);
+        $this->twig->render($extends_template,$context);
     }
 
     // It's possible to render the contents of the parent block by using the parent function.
@@ -226,32 +246,23 @@ class TwigComposerTest extends \PHPUnit_Framework_TestCase
     {
         $extended_template = 'extended_template.twig';
         $extends_template = 'extends_using_parent.twig';
+        $context = self::AN_EXAMPLE_CONTEXT;
 
         $mock_extended = $this->createMockObjectWithACoupleOfMethods(['extended']);
         $mock_extends = $this->createMockObjectWithACoupleOfMethods(['extends']);
 
         $mock_extended->expects($this->once())
-            ->method('extended');
+            ->method('extended')
+            ->with($this->equalTo($context));
 
         $mock_extends->expects($this->once())
-            ->method('extends');
+            ->method('extends')
+            ->with($this->equalTo($context));
 
         TwigComposer::getNotifier()->on($extended_template, [$mock_extended,'extended']);
         TwigComposer::getNotifier()->on($extends_template, [$mock_extends,'extends']);
 
-        $this->twig->render($extends_template);
+        $this->twig->render($extends_template,$context);
     }
-
-
-
-/*
-    public function test_Template()
-    {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-            );
-    }
-*/
-
 
 }
